1d449629598d16fd27e6d3b2f2b6fcbb57e7da8b
jaswanthshakthi <jaswanthshakthi@gmail.com>
2025-03-06 17:32:08 +0530

    COLLECT DATA USING API

--- Data_clct.py
+++ Data_clct.py
@@ -8,10 +8,8 @@ def fetch_sp500_tickers():
     url = "https://en.wikipedia.org/wiki/List_of_S%26P_500_companies"
     tables = pd.read_html(url)
     sp500_df = tables[0]
-    
     tickers = sp500_df['Symbol'].str.strip().tolist()  
     company_names = sp500_df['Security'].str.strip().tolist()  
-    
     return list(zip(tickers, company_names)) 
 
 
